008b795d8533ae74bc5317820abd57c8dbf62c91
Lachlan McDermott <lachlancmcd@gmail.com>
2026-01-26 18:00:42 -0800

    Completed code for lab 1

--- MATLAB/Lab 1/Lab1_GenerateObstacleCoursePlot.m
+++ MATLAB/Lab 1/Lab1_GenerateObstacleCoursePlot.m
@@ -2,34 +2,29 @@ clear;
 clc;
 
 %!!! REPLACE WITH CORRECT TRIAL FILE NUMBERS DURING LAB !!!
-test = dataLog('005');
+test = dataLog('004');
 
+%CALIBRATION
+%!!! REPLACE W/ NUMBERS FROM CALIBRATION !!!
+xZeroOffset = -34.0029;
+yZeroOffset = 100;
+zZeroOffset = 100;
+scaleFactor = 1.2035e-02;
 
-
-%TODO
-
-%convert from teeensy to m/s^2
-%subtract off the arithmatic mean after we do conversion
-%compare with estimated thrust curve thing???
+test.accelX = test.accelX - xZeroOffset;
+test.accelY = test.accelY - yZeroOffset;
+test.accelZ = test.accelZ - zZeroOffset;
 
 %Generate acceleration plots
 figure('Name', 'ROV Acceleration Plot');
+hold on;
+plot(test.accelX*scaleFactor, 'red', 'LineWidth', 1);
+plot(test.accelY*scaleFactor, 'green', 'LineWidth', 1); 
+plot(test.accelZ*scaleFactor, 'blue', 'LineWidth', 1); 
+hold off;
 
-subplot(3,1,1);
-plot(test.accelX, 'red', 'LineWidth', 1);
-title('X Acceleration');
-ylabel('Accel [m/s^2]');
-grid on;
-
-subplot(3,1,2);
-plot(test.accelY, 'green', 'LineWidth', 1);
-title('Y Acceleration');
-ylabel('Accel [m/s^2]');
-grid on;
-
-subplot(3,1,3);
-plot(test.accelZ, 'blue', 'LineWidth', 1);
-title('Z Acceleration');
+title('ROV Acceleration through Obstacle Course');
 ylabel('Accel [m/s^2]');
 xlabel('Time [samples]');
+legend('X Acceleration', 'Y Acceleration', 'Z Acceleration');
 grid on;
