cf5ce9f456fb11fda05e16d7852a5d5dae73a8dc
Lachlan McDermott <lachlancmcd@gmail.com>
2026-01-28 13:24:41 -0800

    more tests

--- MATLAB/Lab 1/Lab1_GenerateObstacleCoursePlot.m
+++ MATLAB/Lab 1/Lab1_GenerateObstacleCoursePlot.m
@@ -2,14 +2,14 @@ clear;
 clc;
 
 %!!! REPLACE WITH CORRECT TRIAL FILE NUMBERS DURING LAB !!!
-test = dataLog('003');
+test = dataLog('023');
 
 %CALIBRATION
 %!!! REPLACE W/ NUMBERS FROM CALIBRATION !!!
-xZeroOffset = 100;
-yZeroOffset = 100;
-zZeroOffset = 100;
-scaleFactor = 1;
+xZeroOffset = -8.2000;
+yZeroOffset = 29.0800;
+zZeroOffset = -12.5120;
+scaleFactor = 0.0097;
 
 test.accelX = test.accelX - xZeroOffset;
 test.accelY = test.accelY - yZeroOffset;
@@ -21,10 +21,11 @@ hold on;
 plot(test.accelX*scaleFactor, 'red', 'LineWidth', 1);
 plot(test.accelY*scaleFactor, 'green', 'LineWidth', 1); 
 plot(test.accelZ*scaleFactor, 'blue', 'LineWidth', 1); 
+xlim([150, 1000]);
 hold off;
 
 title('ROV Acceleration through Obstacle Course');
 ylabel('Accel [m/s^2]');
-xlabel('Time [samples]');
+xlabel('Samples [count]');
 legend('X Acceleration', 'Y Acceleration', 'Z Acceleration');
 grid on;
