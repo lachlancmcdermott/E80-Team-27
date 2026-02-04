3cd5a0c3b7b292bbf003d08864c985cc5a13f6b1
Lachlan McDermott <lachlancmcd@gmail.com>
2026-02-03 21:49:02 -0800

    work

---
+++ MATLAB/Lab 2/dataLog.m
@@ -0,0 +1,58 @@
+%dataLog.m
+%Lachlan McDermott
+%lmcdermott@hmc.edu
+%1/30/2026
+
+%Function takes in generated datalog from Teensy
+%Returns new struct with all data inside, for easy access
+function logData = dataLog(n)
+
+    infofile = strcat('INF', n, '.TXT');
+    datafile = strcat('LOG', n, '.BIN');
+    
+    if ~isfile(infofile) || ~isfile(datafile)
+        error('File %s or %s not found.', infofile, datafile);
+    end
+
+    dataSizes.('float') = 4;
+    dataSizes.('ulong') = 4;
+    dataSizes.('int') = 4;
+    dataSizes.('int32') = 4;
+    dataSizes.('uint8') = 1;
+    dataSizes.('uint16') = 2;
+    dataSizes.('char') = 1;
+    dataSizes.('bool') = 1;
+
+    fileID = fopen(infofile);
+    items = textscan(fileID,'%s','Delimiter',',','EndOfLine','\r\n');
+    fclose(fileID);
+    
+    [ncols,~] = size(items{1});
+    ncols = ncols/2;
+    varNames = items{1}(1:ncols)';
+    varTypes = items{1}(ncols+1:end)';
+    varLengths = zeros(size(varTypes));
+    
+    colLength = 256; 
+
+    for i = 1:numel(varTypes)
+        if isfield(dataSizes, varTypes{i})
+            varLengths(i) = dataSizes.(varTypes{i});
+        else
+            warning('Unknown data type: %s', varTypes{i});
+            varLengths(i) = 0;
+        end
+    end
+
+    fid = fopen(datafile,'rb');
+    
+    logData = struct();
+    
+    for i=1:numel(varTypes)
+        fseek(fid, sum(varLengths(1:i-1)), 'bof');
+        dataCol = fread(fid, Inf, ['*' varTypes{i}], colLength-varLengths(i));
+        logData.(varNames{i}) = dataCol;
+    end
+    
+    fclose(fid);
+end
