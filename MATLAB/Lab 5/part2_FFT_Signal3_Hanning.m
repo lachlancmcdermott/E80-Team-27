1eacda443afae7180a0878afcdd546a196c344dd
Lachlan McDermott <lachlancmcd@gmail.com>
2026-02-23 22:20:08 -0800

    part 2 done

--- MATLAB/Lab 5/part2_FFT_Signal3_Hanning.m
+++ MATLAB/Lab 5/part2_FFT_Signal3_Hanning.m
@@ -1,13 +1,45 @@
 %Lachlan McDermott
-%Lab 5 Underwater accoustics - Part 2 Signal 3 Plot Hanning
-%2/22/2026
+%Lab 5 Underwater acoustics - Part 2 Signal 3 Hanning Comparison
+%2/23/2026
 
-clear; 
 clc;
+clear;
 
-%intake data
-data = 1.5 * sin(2*pi*10000*10000);
+%input data
+%!!!! CHANGE FILE NAME IN LAB !!!!!
+dataFile = 'filefromscopeinlab.csv'; 
+data = readmatrix(dataFile); 
 
-%compute fft
-fft = fft(data);
-fftToPlot = 2 * (abs(fft) / length(data));
+%global variables
+scopeT = data(:, 1);
+scopeV = data(:, 2);
+N = length(scopeV);
+dt = scopeT(2) - scopeT(1);
+fs = 1 / dt;
+
+xAxis = (0:N-1) * (fs / N);
+
+%hann window
+hanning = v_scope .* hann(N);
+
+%fft
+fft = fft(hanning);
+correctionFactor = 2; 
+
+%plot (CHECK LIMITS AND WHATNOT)
+subplot(2,1,1);
+plot(xAxis / 1000, 2 * abs(fft) / N * correctionFactor);
+xlim([0 100]);
+grid on;
+title('Frequency domain');
+xlabel('Frequency [kHz]');
+ylabel('Magnitude [V]');
+
+subplot(2,1,2); 
+plot(t_scope * 1000, hanning);
+%!!!! FIGURE OUT LIMITS FOR PLOTTING IN LAB BASED ON SCOPE DATA !!!!
+xlim([min(t_scope)*1000, max(t_scope)*1000]); 
+grid on;
+title('Time domain');
+xlabel('Time [ms]');
+ylabel('Voltage [V]');
