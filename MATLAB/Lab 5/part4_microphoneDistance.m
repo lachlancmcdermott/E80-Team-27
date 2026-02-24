e2ffa9e4ebfdf94d8dae74cb015c3bb8291b20de
Lachlan McDermott <lachlancmcd@gmail.com>
2026-02-23 23:13:47 -0800

    Update part4_microphoneDistance.m

--- MATLAB/Lab 5/part4_microphoneDistance.m
+++ MATLAB/Lab 5/part4_microphoneDistance.m
@@ -9,7 +9,7 @@ clc;
 %!!!! replace in lab with data from measurements !!!!
 
 distances = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40];
-%spacing rel to the wall
+%spacing rel to the wall and the beacon
 recordedVoltages = [1.80, 1.5, 0.45, 30, 0.98, 0.65, 0.1, 0.2];
 %fft max magnitude 
 
@@ -40,5 +40,5 @@ plot(r, v);
 title('Beacon Received Voltage Magnitude vs. Distance');
 xlabel('Distance from Beacon (abs) [m]');
 ylabel('Received FFT Magnitude [V]');
-equationString = sprintf('Analytical Model (V = %.3f / r)', c);
-legend('Measured Data (w/ Multipath)', equationString);
+equationLegend = sprintf('Analytical Model (V = %.3f / r)', c);
+legend('Measured Data (w/ Multipath)', equationLegend);
